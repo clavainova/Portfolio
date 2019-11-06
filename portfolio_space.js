ccfb584f005172c9b6c8a01bd194a67ad520f0f5
clavainova <clavainova@gmail.com>
2019-11-06 11:03:57 +0100

    cleaner + mobile)

--- portfolio_space.js
+++ portfolio_space.js
@@ -1,3 +1,16 @@
-function drawTimeline(){
-    
+function initialize() {
+    var head = document.head;
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        link.href = "css/portfolio_space_mobile.css";
+        console.log("mobile detected");
+        head.appendChild(link);
+    }
+    else {
+        link.href = "css/portfolio_space_desktop.css";
+        console.log("desktop detected");
+        head.appendChild(link);
+    }
 }
